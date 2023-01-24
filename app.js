a7f82207f3bc0bf4ab064a574c77ddf9f7e13582
Williams <kwilliams31@una.edu>
2023-01-24 16:41:07 -0600

    got views folder ejs setup template engine

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000; 
 
+app.set('view engine', 'ejs');
+
+let myName = 'Kristina';
+
 app.get('/', (req, res) => {
-  res.send('Hello World, are we in november?')
+//   res.send('Hello World, are we in november?')
+    res.send(`<h3> what is up, ${ myName }</h3>`)
 })
 
 app.get('/show', (req, res) => {
     res.sendFile('index.html' , { root : __dirname});
 
 })
+
+app.get('/ejs', (req,res) => {
+    res.render('index');
+
+    // ejs stuff here
+})
+
+
 
 console.log('in the node console');
 
